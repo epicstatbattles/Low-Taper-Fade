a8820ac3454e330769409d41abc0b5450af96f5b
epicstatbattles <158064432+epicstatbattles@users.noreply.github.com>
2025-01-20 10:32:42 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3.14",
+	num: "0.3.14.1",
 	name: "Low Taper Fade Gamma Plus",
 }
 
@@ -82,7 +82,9 @@ let changelog = `<h1>Changelog:</h1><br>
         <h3>v0.3.13</h3><br>
                 - Fixed Ninja upgrade 3 boosting normal point gain.<br>
 	<h3>v0.3.14</h3><br>
-                - Slightly buffed LTF upgrade 6 and massive upgrade 1 to compensate for the "nerf" to progression from the v0.3.13 change.`
+                - Slightly buffed LTF upgrade 6 and massive upgrade 2 to compensate for the "nerf" to progression from the v0.3.13 change.<br>
+	<h3>v0.3.14.1</h3><br>
+                - Fixed previous changelog showing wrong massive upgrade.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
