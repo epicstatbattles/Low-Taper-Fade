88ffb3a0806d95cd1da9088d02928121385cba52
epicstatbattles <158064432+epicstatbattles@users.noreply.github.com>
2025-01-21 14:07:53 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.6.2",
+	num: "0.6.3",
 	name: "Low Taper Fade Zeta",
 }
 
@@ -122,7 +122,9 @@ let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.6.1</h3><br>
                 - Rebalanced the new CT upgrade costs.<br>
 	<h3>v0.6.2</h3><br>
-                - Swapped Aubrinator upgrade effects of 2:2 and 3:1.`
+                - Swapped Aubrinator upgrade effects of 2:2 and 3:1.<br>
+	<h3>v0.6.3</h3><br>
+                - Rebalanced massive upgrade costs.`
 
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
