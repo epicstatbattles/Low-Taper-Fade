92266ab07ab3d456686404b708296171c50f40ea
epicstatbattles <158064432+epicstatbattles@users.noreply.github.com>
2025-01-27 20:52:18 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1570,10 +1570,10 @@ addLayer("infi", {
         21: {
             name: "Anti-Dragging Measures Taken",
             challengeDescription: "You cannot gain Ninja points or Madelizers.",
-            goalDescription: "Reach 1e200 points.",
+            goalDescription: "Reach 1e120 points.",
             rewardDescription: "LTF points and Infinity points boost CT subscriber, Madelizer, and Aubrinator gain.",
             unlocked() { return hasChallenge("infi", 11); },
-            canComplete: function() {return player.points.gte(1e200)},
+            canComplete: function() {return player.points.gte(1e120)},
             rewardEffect() {
                 return player.ltf.points.div(1e100).add(1).pow(0.001).pow(player.infi.points.add(10).log10());
             },
