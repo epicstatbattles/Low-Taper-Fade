2808157814cfb820dd2887ad42d18b44d4353a22
epicstatbattles <158064432+epicstatbattles@users.noreply.github.com>
2025-08-24 17:52:54 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,7 +12,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "4.2.2",
+	num: "4.2.3",
 	name: "The Enchantment Grind",
 }
 
@@ -332,7 +332,9 @@ let changelog = `<h1>Changelog:</h1><br>
  		- Added slowdowns to LTF, Ninja, and massive points at e1800, e1400, and e750 of their point types respectively.<br>
 		- Nerfed several starting values for layer 5 currency upgrades.<br>
   		- Nerfed layer 5 currency gains, as after playtesting, I found out it was extremely OP.<br>
-		- Reduced the cost of galaxies to start at 1e36 IP instead of 1e40 IP.`
+		- Reduced the cost of galaxies to start at 1e36 IP instead of 1e40 IP.<br>
+	<h3>v4.2.3</h3><br>
+ 		- Changed slowdown threshold of LTF, Ninja, and massive points to e2000, e1500, and e800 respectively.`
 
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
