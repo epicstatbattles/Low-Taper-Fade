c017e3a02f4e1ba1a9698066ae4629bc030bed70
epicstatbattles <158064432+epicstatbattles@users.noreply.github.com>
2025-08-31 14:09:03 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2722,7 +2722,7 @@ addLayer("enhance", {
 
             // Effect of the buyable
             effect(x) {
-                return new Decimal(1000).div(new Decimal(1.08).pow(x)); // Apply the diminishing factor
+                return new Decimal(1000).pow(new Decimal(0.984).pow(x)); // Apply the diminishing factor
             },
             canAfford() { return player.enhance.points.gte(this.cost()) },
             buy() {
